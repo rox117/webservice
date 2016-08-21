74a77781e0c5a88a7524b6c0b156136931b6fdb3
rox117 <therox116@gmail.com>
2016-08-21 03:01:39 +0000

    guild bus list added

--- DataModels/GuildRoutes.py
+++ DataModels/GuildRoutes.py
@@ -4,8 +4,8 @@ from flask.ext.mongokit import MongoKit, Document
 class GuildRoute(Document,JsonSerializable):
     """Represents a Route for the Guild bus service"""
     
-    def init(self,title,route,cost,departure_time):
-        self.__title=title
+    def __init__(self,title,route,cost,departure_time):
+        self.__routetitle=title
         self.__route=route
         self.__cost=cost
         self.__departure_time=departure_time
@@ -13,7 +13,7 @@ class GuildRoute(Document,JsonSerializable):
         
         
         structure={
-            "__title":str,
+            "__routetitle":str,
             "__route":str,
             "__cost":str,
             "__departure_time":str
--- DataModels/__init__.py
+++ DataModels/__init__.py
@@ -3,3 +3,4 @@ from JUTC import JRoute
 from Restaurant import Restaurant
 from ShuttleRoute import ShuttleRoute
 from TaxiService import TaxiService
+from GuildRoutes import GuildRoute
